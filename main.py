bc91080a2d143e3503bde2e0f8190f129cdda977
Konstantin_Kalug <kpkalugin@yandex.ru>
2021-04-16 12:16:47 +0700

    work on the map has started

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import requests
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
 from telegram.ext import Updater, MessageHandler, Filters, ConversationHandler
 from telegram.ext import CallbackContext, CommandHandler
@@ -29,6 +30,8 @@ class Bot:
         self.markup_start = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
         reply_keyboard = [['Больше картинок', 'Получить url'], ['Вернуться назад']]
         self.markup_wiki = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
+        reply_keyboard = [['Больше информации'], ['Спутник', 'Гибрид'], ['Вернуться назад']]
+        self.markup_map = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
         updater = Updater(TOKEN, use_context=True)
         dp = updater.dispatcher
         conv_handler = ConversationHandler(
@@ -37,7 +40,9 @@ class Bot:
                 1: [CommandHandler('stop', self.stop), CommandHandler('help', self.help),
                     MessageHandler(Filters.text, self.text_handler_func)],
                 2: [CommandHandler('stop', self.stop),
-                    MessageHandler(Filters.text, self.wiki_handler_func, pass_user_data=True)]
+                    MessageHandler(Filters.text, self.wiki_handler_func, pass_user_data=True)],
+                3: [CommandHandler('stop', self.stop),
+                    MessageHandler(Filters.text, self.map_handler_func, pass_user_data=True)]
             },
             fallbacks=[CommandHandler('stop', self.stop)]
         )
@@ -78,6 +83,10 @@ class Bot:
             update.message.reply_text('Пожалуйста, сообщите мне, что я должен для вас найти!',
                                       reply_markup=self.markup_wiki)
             return 2
+        elif update.message.text == 'YANDEX MAP':
+            update.message.reply_text('Пожалуйста, сообщите мне, что я должен для вас найти!',
+                                      reply_markup=self.markup_map)
+            return 3
 
     def wiki_handler_func(self, update, context):
         # получение материала с википедии стоит перенести в отдельный класс,
@@ -109,6 +118,47 @@ class Bot:
                 update.message.reply_text(wikipage.content[0:4096])
             except Exception:
                 update.message.reply_text('По данному запросу ничего не найдено!')
+
+    def map_handler_func(self, update, context):
+        # получение материала с яндекса стоит перенести в отдельный класс,
+        # когда будет выполнен весь функционал в программе
+        if update.message.text == 'Больше информации' and 'map_req' in context.user_data:
+            pass
+        if update.message.text == 'Вернуться назад':
+            update.message.reply_text('Надеюсь, я помог!', reply_markup=self.markup_start)
+            return 1
+        if update.message.text == 'Спутник' and 'map_req' in context.user_data.keys():
+            l_map = 'sat'
+        elif update.message.text == 'Гибрид' and 'map_req' in context.user_data.keys():
+            l_map = 'sat,skl'
+        else:
+            l_map = 'map'
+            context.user_data['map_req'] = update.message.text
+        try:
+            print(context.user_data['map_req'])
+            geocoder_uri = geocoder_request_template = "http://geocode-maps.yandex.ru/1.x/"
+            response = requests.get(geocoder_uri, params={
+                "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+                "format": "json",
+                "geocode": context.user_data['map_req']
+            })
+            toponym = response.json()["response"]["GeoObjectCollection"][
+                "featureMember"][0]["GeoObject"]
+            ll, spn = self.get_ll_spn(toponym)
+            static_api_request = f"http://static-maps.yandex.ru/1.x/?ll={ll}&spn={spn}&l={l_map}"
+            context.bot.send_photo(
+                update.message.chat_id,
+                static_api_request,
+                caption=""
+            )
+        except Exception:
+            update.message.reply_text("По данному запросу ничего не найдено!")
+
+    def get_ll_spn(self, toponym):
+        ll = ','.join(toponym["Point"]["pos"].split())
+        spn = ','.join([str(float(toponym["boundedBy"]["Envelope"]["upperCorner"].split()[0]) - float(toponym["boundedBy"]["Envelope"]["lowerCorner"].split()[0])),
+                        str(float(toponym["boundedBy"]["Envelope"]["upperCorner"].split()[1]) - float(toponym["boundedBy"]["Envelope"]["lowerCorner"].split()[1]))])
+        return ll, spn
 
 
 if __name__ == '__main__':
